ea56fbaa05c6428e762da0719d7272c9edbff167
Magnus Mengelbier <magnus.mengelbier@gmail.com>
2025-03-05 12:39:38 +0000

    added job and action context as types

--- tests/testthat/test-internal-referenceid.R
+++ tests/testthat/test-internal-referenceid.R
@@ -40,6 +40,42 @@ testthat::test_that( "referenceid.typeNull", {
 
 
 
+
+testthat::test_that( "referenceid.typeAction", {
+  
+  # -- stage
+  
+  # look at uniqueness across 10 000 IDs
+  test_length <- 10000
+  
+  
+  # -- test
+  result <- replicate( test_length, 
+                       cxlib:::.cxlib_referenceid( type = "action" ), 
+                       simplify = TRUE)
+  
+  
+  
+  # -- expected
+  
+  expected_length <- test_length
+  
+  # note: format is 5 repeating blocks of 8 characters a-z and digits 0-9 delimited by a dash
+  expected_format <- "^([a-f0-9]{8}-){4}[a-f0-9]{8}$"
+  
+  # -- assertions
+  
+  # uniqueness
+  testthat::expect_equal( length(base::unique(result)), expected_length )
+  
+  # format
+  testthat::expect_true(all( grepl( expected_format, result) ))
+  
+  
+})
+
+
+
 testthat::test_that( "referenceid.typeSHA", {
   
   # -- stage
@@ -124,6 +160,42 @@ testthat::test_that( "referenceid.typeUUID", {
   # -- test
   result <- replicate( test_length, 
                        cxlib:::.cxlib_referenceid( type = "uuid" ), 
+                       simplify = TRUE)
+  
+  
+  
+  # -- expected
+  
+  expected_length <- test_length
+  
+  # note: format is a squence of 40 characters a-z and digits 0-9 
+  # example: bcf1321b-7560-4124-8f29-8f6f3c35296f
+  expected_format <- "^[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12}$"
+  
+  # -- assertions
+  
+  # uniqueness
+  testthat::expect_equal( length(base::unique(result)), expected_length )
+  
+  # format
+  testthat::expect_true(all( grepl( expected_format, result) ))
+  
+  
+})
+
+
+
+testthat::test_that( "referenceid.typeJob", {
+  
+  # -- stage
+  
+  # look at uniqueness across 10 000 IDs
+  test_length <- 10000
+  
+  
+  # -- test
+  result <- replicate( test_length, 
+                       cxlib:::.cxlib_referenceid( type = "job" ), 
                        simplify = TRUE)
   
   
